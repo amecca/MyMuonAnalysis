14887ed669e56645f559717886abe77fa722c745
Alberto <alberto.mecca@cern.ch>
2023-07-19 12:09:53 +0200

    Command for cmsRun exits with cmsRun's status

--- DisplacedMuons/test/command-cmsRun.sh
+++ DisplacedMuons/test/command-cmsRun.sh
@@ -1,1 +1,8 @@
-cmsRun recoMuonGeneralAnalyzer_cfg.py -n 1 2>&1 | gzip --fast > cmsrun.log.gz; bell-ring
+#!/bin/sh
+
+set -o pipefail
+
+cmsRun recoMuonGeneralAnalyzer_cfg.py -n 1 2>&1 | gzip --fast > cmsrun.log.gz
+exitStatus=$?
+bell-ring
+exit $exitStatus
